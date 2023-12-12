d421354adc7c4069d52e44d53936f07f29387a81
AP-Yroki <144231060+AP-Yroki@users.noreply.github.com>
2023-12-12 12:05:28 +0300

    Add files via upload

---
+++ app/views.py
@@ -0,0 +1,18 @@
+# app/views.py
+from django.shortcuts import render, redirect
+from .models import News
+from .forms import NewsForm
+
+def news_list(request):
+    news = News.objects.all()
+    return render(request, 'news_list.html', {'news': news})
+
+def add_news(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('news_list')
+    else:
+        form = NewsForm()
+    return render(request, 'add_news.html', {'form': form})
